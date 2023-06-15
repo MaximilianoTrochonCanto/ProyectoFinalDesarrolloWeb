d5b9b6d35b789d58e766fd4f4d31a93e6b4a8249
Maximiliano Trochon Canto <maxitc8@hotmail.com>
2023-06-14 22:21:17 -0300

    primer commit: proyecto final

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 function verMas(numero,parrafo){
     
-    localStorage.setItem("Titulo",document.querySelector('.blog-article:nth-child('+numero+') h1').innerHTML);
+    localStorage.setItem("Titulo",document.querySelector('.blog-article:nth-child('+numero+') h2').innerHTML);
     localStorage.setItem("Parrafo",document.querySelector('.blog-article:nth-child('+numero+') p').innerHTML);    
     localStorage.setItem("Parrafo2",parrafo);
     window.location.href = "blogArticle.html"
